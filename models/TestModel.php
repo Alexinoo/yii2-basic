43d0f426ca2379ed522efeba9416a297b9ef7d41
Alex Mwangi <alex@stl-horizon.com>
2021-12-01 15:59:32 +0300

    Override attributeLabels() to explicitly declare attribute labels

--- models/TestModel.php
+++ models/TestModel.php
@@ -8,5 +8,17 @@ class TestModel extends \yii\base\Model
     public $name;
     public $surname;
     public $email;
+    public $myAge;
+
+    // Override yii\base\Model::attributeLabels() to explicitly declare attribute labels.
+    // Returns an associative array with key pair attributes
+    public function attributeLabels()
+    {
+        return [
+            'name' => 'Your name',
+            'email' => 'Your email address',
+            'myAge' => 'Your Age',
+        ];
+    }
 
 }
