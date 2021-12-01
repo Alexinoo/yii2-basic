7dfde257efb3193b9c352154d064acd2fd99e99b
Alex Mwangi <alex@stl-horizon.com>
2021-12-01 15:22:13 +0300

    Change browser titles / meta tags using  - Yii\web\View

--- views/page/page_footer.php
+++ views/page/page_footer.php
@@ -1,5 +1,19 @@
+<?php
+/**
+ *   Means variable $this is an instance of \yii\web\View
+ * @var \yii\web\View $this
+ * 
+ */
+
+$this->title =' About Us';
+
+$this->registerMetaTag(['name' => 'keywords' , 'content' => 'yii2']);
+
+?>
+
 <h1>  Page Controller's footer..</h1>
 
 <div >
-    <?php echo  $this->params['sharedVariable'] ;?>
+    <?php echo  Yii::$app->view->params['sharedVariable'] ;?>
 </div>
+
